da02a6851e8087fc9f80fbb8984e60c921dc6e39
Jon Moss <me@jonathanmoss.me>
2020-07-01 21:45:24 -0400

    Add custom admin for Case

--- evictionproject/evictions/admin.py
+++ evictionproject/evictions/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Case, Court
 
-admin.site.register(Case)
 admin.site.register(Court)
+
+class CaseAdmin(admin.ModelAdmin):
+    list_display = ['ujs_id', 'court', 'status', 'defendant', 'plaintiff']
+    list_filter = ['court', 'defendant_zipcode', 'plaintiff_zipcode']
+    search_fields = ['court__id', 'defendant', 'plaintiff']
+
+admin.site.register(Case, CaseAdmin)
--- evictionproject/evictions/models.py
+++ evictionproject/evictions/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Court(models.Model):
     friendly_court_id = models.CharField(max_length=200)
     court_id = models.CharField(max_length=200)

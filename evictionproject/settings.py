591ba16c2a0621e0084d6c0285e0a5fe360a0398
Jon Moss <me@jonathanmoss.me>
2020-07-08 14:20:12 -0400

    Check SECRET_KEY env var for key first

--- evictionproject/settings.py
+++ evictionproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'aw8&qp@b)a$=_amaswae$4gdnse1^+u!e+_+gv-lmx6uw3vs@k'
+SECRET_KEY = os.getenv('SECRET_KEY', 'aw8&qp@b)a$=_amaswae$4gdnse1^+u!e+_+gv-lmx6uw3vs@k')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

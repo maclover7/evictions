31d1d9fb5c2483a916db154410d02b88d2353f4e
Jon Moss <me@jonathanmoss.me>
2020-07-29 14:42:05 -0400

    Fix ALLOWED_HOSTS

--- evictionproject/settings.py
+++ evictionproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'aw8&qp@b)a$=_amaswae$4gdnse1^+u!e+_+gv-lmx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
